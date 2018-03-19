e059eee74ebeffe6756d694a588596c5b706c156
Niklas Grieger <niklas.grieger@ottogroup.com>
2018-03-19 17:13:34 +0100

    new splash image

--- app/src/main/java/devnik/trancefestivalticker/activity/SplashActivity.java
+++ app/src/main/java/devnik/trancefestivalticker/activity/SplashActivity.java
@@ -16,8 +16,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+        //finish();
     }
 }
